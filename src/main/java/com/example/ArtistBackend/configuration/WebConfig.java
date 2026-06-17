b5b5e9bdc2f554cdaecf183824b7e8c14b7b992b
siddhesh-jeughale <siddheshjeughale7000@gmail.com>
2026-06-17 21:33:22 +0530

    add some config

--- src/main/java/com/example/ArtistBackend/configuration/WebConfig.java
+++ src/main/java/com/example/ArtistBackend/configuration/WebConfig.java
@@ -1,9 +1,13 @@
 package com.example.ArtistBackend.configuration;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+import java.util.Arrays;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -19,5 +23,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceHandler("/images/**")
 //                .addResourceLocations("file:./uploads/");
                 .addResourceLocations("file:" + uploadDir + "/");
+    }
+
+    @PostConstruct
+    public void checkUploads() {
+        File uploadFolder = new File("./uploads");
+
+        System.out.println("Uploads path: " + uploadFolder.getAbsolutePath());
+        System.out.println("Uploads exists: " + uploadFolder.exists());
+
+        if (uploadFolder.exists()) {
+            System.out.println(Arrays.toString(uploadFolder.list()));
+        }
     }
 }
